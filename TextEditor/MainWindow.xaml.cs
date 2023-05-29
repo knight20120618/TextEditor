73e639fc0b30dcfacb7d1a91892bd2ab237086ce
knight20120618 <125259069+knight20120618@users.noreply.github.com>
2023-05-29 14:57:29 +0800

    程式碼撰寫
    1.開啟與儲存RTF檔案
    2.使用下拉選單來改變字型與字體大小

--- TextEditor/MainWindow.xaml.cs
+++ TextEditor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO; // 要引用這個IO函式庫，才能作檔案處理
 
 namespace TextEditor
 {
@@ -23,6 +24,53 @@ namespace TextEditor
         public MainWindow()
         {
             InitializeComponent();
+            // 清除rtbText的內容
+            rtbText.Document.Blocks.Clear();
+            // 設定字型下拉選單的選單內容，存取你的電腦裡面的字型庫，將你安裝的字型清單都放進去
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            // 設定字體大小下拉選單的選單內容，設定8`72的數字，這要用來設定字體大小
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            // 跟記事本範例程式類似，不過要改成過濾為RTF檔案格式
+            dlg.Filter = "RTF文件 (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd);
+                // DataFormats 檔案格式也要設定為RTF檔案格式
+                range.Load(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.Filter = "RTF文件 (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                TextRange range = new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // 判斷式：必須要有選擇項目，才會做文字格式改變
+            if (cmbFontFamily.SelectedItem != null)
+                // 將rtbText豐富文字框所選的項目，套用所設定的字型
+                rtbText.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        private void cmbFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontSize.SelectedItem != null)
+                // 將rtbText豐富文字框所選的項目，套用所設定的字體大小
+                rtbText.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.SelectedItem);
         }
     }
 }
